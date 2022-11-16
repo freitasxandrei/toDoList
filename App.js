5cb04e26b3a02d0d2e5900e66c764dc4378a53c8
andrei <andrei@iepro.com.br>
2022-11-16 20:59:01 -0300

    newFields

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 import {
   Keyboard,
@@ -12,14 +13,49 @@ import {
   View,
   TextInput,
   TouchableOpacity,
-  FlatList
+  FlatList,
+  Button,
 } from "react-native";
 
 export default function App() {
 
-  const [task, setTask] = useState([]);
+  const local = 'pt-br';
+
+  // const [dadosTask, setDadosTask] = useState(estadoInicial)
+
+  // const estadoInicial = {
+  //   name: '',
+  //   priority: -1,
+  // }
+
+  const [task, setTask] = useState([
+
+    // {
+    //   name: '',
+    //   priority: -1,
+    // }
+
+  ]);
+
+  // function resetForm() {
+  //   setTask(estadoInicial)
+  // }
 
   const [newTask, setNewTask] = useState("");
+
+  const [name, setName] = useState();
+
+  const [datePicker, setDatePicker] = useState(false);
+
+  const [date, setDate] = useState(new Date());
+
+  const [dates, setDates] = useState(new Date());
+
+  const [timePicker, setTimePicker] = useState(false);
+
+  const [time, setTime] = useState(new Date(Date.now()));
+
+  const [priority, setPriority] = useState();
 
   async function addTask() {
     const search = task.filter(task => task === newTask);
@@ -29,9 +65,13 @@ export default function App() {
       return;
     }
 
+    // setTask([...dadosTask, setDadosTask]);
+
+
     setTask([...task, newTask]);
-    
+
     setNewTask("");
+    setPriority("");
 
     Keyboard.dismiss();
   }
@@ -56,6 +96,23 @@ export default function App() {
       { cancelable: false }
     );
   }
+
+  function showDatePicker() {
+    setDatePicker(true);
+  };
+
+  function onDateSelected(event, value) {
+    setDate(value);
+    setDatePicker(false);
+  };
+
+  function onTimeSelected(event, value) {
+    setTime(value);
+    setTimePicker(false);
+  };
+
+  function pad(n) { return n < 10 ? "0" + n : n; }
+  var formatDate = pad(date.getDate()) + "/" + pad(date.getMonth() + 1) + "/" + date.getFullYear();
 
   useEffect(() => {
     async function carregaDados() {
@@ -92,7 +149,10 @@ export default function App() {
               style={styles.FlatList}
               renderItem={({ item }) => (
                 <View style={styles.ContainerView}>
+
+                  {/* {task.map(item => { item.name })} */}
                   <Text style={styles.Texto}>{item}</Text>
+
                   <TouchableOpacity onPress={() => removeTask(item)}>
                     <MaterialIcons
                       name="delete-forever"
@@ -107,14 +167,44 @@ export default function App() {
 
           <View style={styles.Form}>
             <TextInput
+              name="name"
               style={styles.Input}
-              placeholderTextColor="#999"
               autoCorrect={true}
               value={newTask}
-              placeholder="Adicione uma tarefa"
+              placeholder="Título"
               maxLength={25}
               onChangeText={text => setNewTask(text)}
             />
+
+            <TextInput
+              name="priority"
+              placeholder="Prioridade"
+              style={styles.InputPrioridade}
+              autoFocus={true}
+              onChangeText={(text) => setPriority(text)}
+              value={priority}
+              keyboardType='numeric'
+              maxLength={1}
+              range
+            />
+
+            {datePicker && (
+              <DateTimePicker
+                name="date"
+                value={date}
+                mode={'date'}
+                display={Platform.OS === 'ios' ? 'spinner' : 'default'}
+                is24Hour={true}
+                onChange={onDateSelected}
+              />
+            )}
+
+            {!datePicker && (
+              <View style={{ marginLeft: 10, width: 130, marginTop: 2, }}>
+                <Button title="Escolher Data" style={styles.button} onPress={showDatePicker} />
+              </View>
+            )}
+
             <TouchableOpacity style={styles.Button} onPress={() => addTask()}>
               <Ionicons name="ios-add" size={20} color="white" />
             </TouchableOpacity>
@@ -148,6 +238,17 @@ const styles = StyleSheet.create({
   },
   Input: {
     flex: 1,
+    height: 40,
+    backgroundColor: "#eee",
+    borderRadius: 4,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: "#eee"
+  },
+  InputPrioridade: {
+    flex: 1,
+    marginLeft: 10,
     height: 40,
     backgroundColor: "#eee",
     borderRadius: 4,
